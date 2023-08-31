ec092a0432e6b5688ca07d2c6e5e5492a7f01c3c
E-Mello <e.mello.dev@gmail.com>
2023-08-30 22:02:19 -0400

    I redid the app to be in accordance with the subbase, I created it based on the template that the subbase offers

---
+++ lib/supabase.ts
@@ -0,0 +1,31 @@
+import 'react-native-url-polyfill/auto'
+
+import * as SecureStore from 'expo-secure-store'
+
+import {DATABASE_URL, PUBLIC_ANON_KEY} from 'react-native-dotenv'
+
+import { createClient } from '@supabase/supabase-js'
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key)
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value)
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key)
+  },
+}
+
+const supabaseUrl = DATABASE_URL
+const supabaseAnonKey = PUBLIC_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
